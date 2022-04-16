3438bbb2781b2fbcb31cdc5e5eabc28ab560c2d2
Lee <374447589@qq.com>
2022-04-16 10:17:51 +0800

    完成了首页功能的开发

--- main.js
+++ main.js
@@ -1,7 +1,38 @@
 
 // #ifndef VUE3
 import Vue from 'vue'
+
 import App from './App'
+
+//导入网络请求的包
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+
+//请求的根路径
+$http.baseUrl = 'http://www.uinav.com'
+
+//请求拦截器
+$http.beforeRequest = function(options){
+	uni.showLoading({
+		title:'数据加载中...'
+	})
+}
+
+// 响应拦截器
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+
+// 封装的展示消息提示的方法
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
+
 
 Vue.config.productionTip = false
 
